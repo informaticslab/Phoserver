60035c693be5dcd95dd7cdbeddbefed67727896f
pwhitebe <pwhitebe@gmail.com>
2014-01-15 15:26:39 -0500

    doing initial load, will need to add data folder as needed.
    Install Node.JS for your system.
    Navigate to the containing folder for this project
    do a npm install
    install mongodb
    in a separate terminal window, start mongodb to the test database with…
    ./mongod --dbpath ~/Documents/Code/photon-server/data
    
    Then, you can ./mongo into the database as needed,
    then if you run
    node app.js
    then it will be running and you can test adding and subtracting users
    and all that good stuff.

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+// New Code, added by ME :D :D happy...
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/test-photon-admin');
+
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+//Fun added code by ME :D
+app.get('/helloworld', routes.helloworld);
+app.get('/userlist', routes.userlist(db));
+app.get('/newuser', routes.newuser);
+app.get('/checkuser', routes.checkuser);
+app.post('/adduser', routes.adduser(db));
+app.post('/verifyuser', routes.verifyuser(db));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
