8f45e00f5fd7c5509687ce7df52e9833569a6c53
pwhitebe <pwhitebe@gmail.com>
2014-01-22 12:20:30 -0500

    Updated with passport local
    Not crashing, not wired to anything yet.

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+//added security goodness
+passport = require("passport");
+LocalStrategy = require('passport-local').Strategy;
+
 // New Code, added by ME :D :D happy...
 var mongo = require('mongodb');
 var monk = require('monk');
@@ -28,9 +32,15 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({ secret: 'SECRET' }));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+//added forsecurity environment'
+app.use(express.bodyParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // development only
 if ('development' === app.get('env')) {
@@ -47,6 +57,50 @@ app.get('/newuser', routes.newuser);
 app.get('/checkuser', routes.checkuser);
 app.post('/adduser', routes.adduser(db));
 app.post('/verifyuser', routes.verifyuser(db));
+
+
+passport.use(new LocalStrategy(function(username, password,done){
+    var collection = db.get('usercollection');
+         collection.findOne({username:username}, function(e,doc){
+           if(e) { return done(e); }
+        if(!doc){
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+           var bcrypt = require('bcrypt-nodejs');
+//           if(bcrypt.compareSync(password, doc.passhash))
+//           {
+//               res.send("That was the correct password :D");
+//               //console.log("The compareSync function returned true");
+//           }
+//           else if(!bcrypt.compareSync(userPassword, doc.passhash))
+//           {
+//               res.send("WHO YOU IS AMS!? >:C");
+//               //console.log("The compareSync function did not return true");
+//           };
+           bcrypt.compare(password, doc.passhash, function(err, res) {
+               if (err) { return done(err); }
+               if (res === true)
+               {
+                    return done(null, doc);
+                }
+                done(null, false, { message: 'Incorrect password.' });
+    // res === true
+     });
+         })}));
+     //  });
+//    db.users.findOne({ username : username},function(err,user){
+//        if(err) { return done(err); }
+//        if(!user){
+//            return done(null, false, { message: 'Incorrect username.' });
+//        }
+//
+//        hash( password, user.salt, function (err, hash) {
+//            if (err) { return done(err); }
+//            if (hash == user.hash) return done(null, user);
+//            done(null, false, { message: 'Incorrect password.' });
+//        });
+//    });
+//}));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
