29834b9e5f9650a11f4adf516cf5a891ce4f0fce
pwhitebe <pwhitebe@gmail.com>
2014-01-27 09:05:35 -0500

    Added bearer strategy mechanics
    Added the bearer strategy mechanics so that the non-ejs apps can login
    with a token  Got it building but needs more testing.  Also updated
    packages so that it would build on my windows 8 system.

--- config/pass.js
+++ config/pass.js
@@ -1,5 +1,6 @@
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy
+  , BearerStrategy = require('passport-http-bearer').Strategy
   , db = require('./dbschema');
 
 passport.serializeUser(function(user, done) {
@@ -77,3 +78,74 @@ exports.ensureAdmin = function ensureAdmin(req, res, next) {
             res.send(403);
     }
 }
+
+// Use the BearerStrategy within Passport.
+//   Strategies in Passport require a `validate` function, which accept
+//   credentials (in this case, a token), and invoke a callback with a user
+//   object.
+passport.use(new BearerStrategy({
+  },
+  function(token, done) {
+    // asynchronous validation, for effect...
+    process.nextTick(function () {
+      
+      // Find the user by token.  If there is no user with the given token, set
+      // the user to `false` to indicate failure.  Otherwise, return the
+      // authenticated `user`.  Note that in a production-ready application, one
+      // would want to validate the token for authenticity.
+       
+       db.tokenModel.findOne({token:token}, function(err, token){ 
+              if(err) {return done(err);}
+              if(!token) {
+//                                var newtoken = new db.tokenModel({ username: username		    
+//                                            ,token: Date.now});
+//                  newtoken.save(function(err) {
+//                        if(err) {
+//                    console.log('Error: ' + err);
+//                    return done(false);
+                        console.log("Nonexistant token " + token);
+                    return done(null, false, { message: 'Nonexistant token.' });
+                    } else {
+                        db.userModel.findOne({username:token.username}, function(err, user){
+                        console.log('saved new token: ' + newtoken.username);
+                        if(err) {return done(err);}
+                        if(!user){return done(null, false, {message: "Token exists but user doesn't"})}
+                        })
+                        //done();
+                    }
+                     return done(null, user);
+                }); 
+              
+       
+       
+       
+       
+       
+       
+      // 
+//      findByToken(token, function(err, user) {
+//        if (err) { return done(err); }
+//        if (!user) { return done(null, false); }
+////        return done(null, user);
+//      })
+    });
+  }
+));
+
+//function findByToken(token, fn) {
+////  for (var i = 0, len = users.length; i < len; i++) {
+////    var user = users[i];
+////    if (user.token === token) {
+////      return fn(null, user);
+////    }
+////  }
+////  return fn(null, null);
+//userdb.findOne({ token : token},function(err,user, done){
+//    console.log("token was " + token)
+//        if(err) { return done(err); }
+//        if(!user){
+//            return done(null, false, { message: 'Nonexistant token.' });
+//        }
+//        return user;
+//})
+//};
