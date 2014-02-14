efb5750de7201c0dcddef54e8c1d0ea85d7fc759
pwhitebe <pwhitebe@gmail.com>
2014-02-13 21:19:01 -0500

    updated for Cross Origen Resource Sharing
    This should probably be pushed into Phoserver as well.

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.configure(function() {
   app.use(express.static(__dirname + '/../../public'));
 });
 
-
+//enable CORS
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 // Basic pages
 app.get('/', basic_routes.index);
 
